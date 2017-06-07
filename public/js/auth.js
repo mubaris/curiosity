e92d863b84875b93f494f9b6523ed1ef1f4605b3
Ashok Siyani <ashok.siyani@gmail.com>
2017-06-07 21:36:04 +0100

    changed login alert - now it will ask for login

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,23 +2,14 @@ const login_button = document.getElementById('login_button');
 const logout_button = document.getElementById('logout_button');
 const loggedIn = document.getElementById('loggedIn');
 
-axios({
-    url: '/user',
-    method: 'get',
-    responseType: 'json',
-}).then((response) => {
-    console.log(response);
-    if (response.data) {
-        loggedIn.style.display = 'inline-block';
-        document.getElementById('userName').innerText = `Hi! ${response.data.name}`;
-        document.getElementById('userName').href = response.data.html_url;
-        return;
-    }
-    login_button.style.display = 'inline-block';
-}).catch((err) => {
-    login_button.style.display = 'inline-block';
-    console.error(err);
-});
+function getUserDetails() {
+    return axios({
+        url: '/user',
+        method: 'get',
+        responseType: 'json',
+    });
+}
+
 
 logout_button.addEventListener('click', () => {
     axios({
