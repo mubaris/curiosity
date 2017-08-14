a27a7974ba1a7bb7b94ce5e53e28887764134cf6
AvaniVerma <avani.verma1998@gmail.com>
2017-08-14 15:31:08 +0530

    Update usernameSelection.js

--- js/usernameSelection.js
+++ js/usernameSelection.js
@@ -1,4 +1,5 @@
 const MAX_USERNAMES_SHOWING = 5;
+let showingAllUsernames = true;
 
 const addUsername = function addUsername() {
     swal({
@@ -13,11 +14,12 @@ const addUsername = function addUsername() {
                 setTimeout(() => {
                     if (username == '') {
                         reject('Username cannot be null.');
-                    } else if (JSON.parse(localStorage.getItem('usernames')).indexOf(username) > -1) {
+                    } else if (USERNAMES.indexOf(username) > -1) {
                         reject('Username already exists! Please add a different username.');
                     } else {
                         // check for valid username
-                        const url = `https://api.github.com/users/${username}`;
+                        let accessToken = localStorage.getItem('accessToken');
+                        const url = `https://api.github.com/users/${username}?access_token=${accessToken}`;
                         axios({
                             url,
                             method: 'get',
@@ -42,21 +44,20 @@ const addUsername = function addUsername() {
 };
 
 const addOneUsername = function addOneUsername(username) {
-    let usernames = JSON.parse(localStorage.getItem('usernames'));
-    usernames.push(username);
-    localStorage.setItem('usernames', JSON.stringify(usernames));
+    USERNAMES.push(username);
+    localStorage.setItem('usernames', JSON.stringify(USERNAMES));
     content.innerHTML = '';
     getData();
     renderUsernames();
 };
 
+const inputOptions = new Promise((resolve) => {
+    setTimeout(() => {
+        resolve(USERNAMES);
+    }, 2000);
+});
+
 const removeUsername = function removeUsername() {
-    inputOptions = new Promise((resolve) => {
-        let usernames = JSON.parse(localStorage.getItem('usernames'));
-        setTimeout(() => {
-            resolve(usernames);
-        }, 2000);
-    });
     swal({
         title: 'Select username to remove',
         input: 'radio',
@@ -73,52 +74,58 @@ const removeUsername = function removeUsername() {
             });
         },
     }).then((index) => {
-        let userNameToRemove = removeUsernameAtIndex(index);
         swal({
             type: 'success',
-            html: `You successfully removed ${userNameToRemove}`,
+            html: `You successfully removed ${USERNAMES[index]}`,
         });
+        removeUsernameAtIndex(index);
     });
 };
 
-function removeUsernameAtIndex(index) {
-    let usernames = JSON.parse(localStorage.getItem('usernames'));
-    let userNameToRemove = usernames[index];
-    usernames.splice(index, 1);
-    localStorage.setItem('usernames', JSON.stringify(usernames));
+const removeUsernameAtIndex = function removeUsernameAtIndex(index) {
+    USERNAMES.splice(index, 1);
+    localStorage.setItem('usernames', JSON.stringify(USERNAMES));
     content.innerHTML = '';
     getData();
     renderUsernames();
-    return userNameToRemove;
-}
+};
 
 const showAllUsernames = function showAllUsernames() {
-    let usernameSelector = document.getElementById('username_selector');            
-    usernameSelector.style.display = usernameSelector.style.display === 'none' ? 'block' : 'none';
+    showingAllUsernames = true;
+    renderUsernames();
+};
+
+const showLessUsernames = function showLessUsernames() {
+    showingAllUsernames = false;
+    renderUsernames();
+};
+
+const renderShowMoreLessUsernames = function renderShowMoreLessUsernames() {
+    const moreLessUsernamesElement = document.getElementById('showMoreLessUsernames');
+    if (showingAllUsernames) { moreLessUsernamesElement.innerHTML = '<a href=javascript:showLessUsernames()><strong>Show less usernames</strong></a>'; } else { moreLessUsernamesElement.innerHTML = '<a href=javascript:showAllUsernames()><strong>Show more usernames</strong></a>'; }
 };
 
 const generateUsernameSelector = function generateUsernameSelector() {
     let usernameSelector = '';
     let i = 0;
-    let usernames = JSON.parse(localStorage.getItem('usernames'));
-    for (; i < usernames.length - 1; i += 1) {
-        usernameSelector += `<a class='selectors' href='https://github.com/${usernames[i]}?tab=stars'>${usernames[i]}</a>`;
-        usernameSelector += ' | ';
-    }        
-    usernameSelector += `<a class='selectors' href='https://github.com/${usernames[i]}?tab=stars'>${usernames[i]}</a>`;
+    if (showingAllUsernames || USERNAMES.length <= MAX_USERNAMES_SHOWING) {
+        for (; i < USERNAMES.length - 1; i += 1) {
+            usernameSelector += `<a class='selectors' href='https://github.com/${USERNAMES[i]}?tab=stars'>${USERNAMES[i]}</a>`;
+            usernameSelector += ' | ';
+        }
+    } else {
+        for (; i < MAX_USERNAMES_SHOWING - 1; i += 1) {
+            usernameSelector += `<a class='selectors' href='https://github.com/${USERNAMES[i]}?tab=stars'>${USERNAMES[i]}</a>`;
+            usernameSelector += ' | ';
+        }
+    }
+    usernameSelector += `<a class='selectors' href='https://github.com/${USERNAMES[i]}?tab=stars'>${USERNAMES[i]}</a>`;
     return usernameSelector;
 };
 
 const renderUsernames = function renderUsernames() {
     document.getElementById('username_selector').innerHTML = generateUsernameSelector();
-    document.getElementById('addUsername').setAttribute('href', 'javascript:addUsername()');     
-    document.getElementById('removeUsername').setAttribute('href', 'javascript:removeUsername()'); 
-    document.getElementById('showUsernames').setAttribute('href', 'javascript:showAllUsernames()'); 
-
-    document.getElementById('settings').addEventListener('click', renderUsernamesHandler)
+    document.getElementById('addOrRemoveUsername').innerHTML = "<a class='addOrRemoveUsername' href=javascript:addUsername()><strong>Add more username</strong></a>" +
+        " | <a class='addOrRemoveUsername' href=javascript:removeUsername()><strong>Remove username</strong></a>";
+    renderShowMoreLessUsernames();
 };
-
-const renderUsernamesHandler = function renderUsernamesHandler() {
-    let settings = document.getElementById('dropdown_content');
-    settings.style.display = settings.style.display == (null || 'none') ? 'block' : 'none';
-}
